bfa4710d2cf6955de5efeae0ba588b053d1933e7
andrewnewson <131465681+andrewnewson@users.noreply.github.com>
2025-01-24 16:40:31 +0000

    Update yolo_inference.py
    - change to run on video
    - print results

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,4 +2,11 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-model.predict('input_videos/image.png', save=True)
+result = model.predict('input_videos/input_video.mp4', save=True)
+print(result)
+print("------------------")
+print("------------------")
+print("Boxes:")
+for box in result[0].boxes:
+    print(box)
+    print("------------------")
