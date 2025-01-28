3348a1798b304af49c4972fb7e9ce814680eea32
andrewnewson <131465681+andrewnewson@users.noreply.github.com>
2025-01-28 11:58:10 +0000

    Update yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolov5_ball_best.pt')
+#model = YOLO('models/yolov5_ball_best.pt')
+model = YOLO('yolov8x.pt')
 
-result = model.predict('input_media/input_video.mp4', conf=0.2, save=True)
-print(result)
-print("------------------")
-print("------------------")
-print("Boxes:")
-for box in result[0].boxes:
-    print(box)
-    print("------------------")
+result = model.track('input_media/input_video.mp4', conf=0.2, save=True)
+# print(result)
+# print("------------------")
+# print("------------------")
+# print("Boxes:")
+# for box in result[0].boxes:
+#     print(box)
+#     print("------------------")
